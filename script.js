ec2566e4762a76c61494487d9877e7d9343905ac
SutonToch <77286229+SutonToch@users.noreply.github.com>
2023-08-05 20:43:42 +0200

    UPDATE use eventListener instead of onClick

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+const ratingScores = document.querySelectorAll("div.rating-score")
+const submitBtn = document.querySelector(".btn");
 let chosenRating = 0;
+
+
+
+ratingScores.forEach(score => {
+    score.addEventListener("click", function() {
+        chooseRating(score)
+    })
+})
+
+submitBtn.addEventListener("click", function() {
+    submit()
+})
+
+
 
 function chooseRating(el) {
     if(chosenRating != 0) {
         document.querySelector(".rating-score-selected").classList.remove("rating-score-selected");
     }
-    el.classList.toggle("rating-score-selected");
+    el.classList.add("rating-score-selected");
     chosenRating = el.textContent;
 }
 
